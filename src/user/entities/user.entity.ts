ba278ea4be9b8ea146da14ff666af081c87660f4
Beardevelope <mck84835182@gmail.com>
2024-01-17 20:55:58 +0900

    feat/user 관련 기능 추가, 회원가입, 수정, 정보조회, 상세찾기, softdelete

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,8 @@
+import { Exclude } from 'class-transformer';
 import { IsEmail, IsNotEmpty, IsNumber, IsString, MinLength } from 'class-validator';
 import { Banner } from 'src/banner/entities/banner.entity';
 import { Comment } from 'src/comment/entities/comment.entity';
+import { Company } from 'src/company/entity/company.entity';
 import { Post } from 'src/post/entities/post.entity';
 import { RecruitPost } from 'src/recruit-post/entities/recruit-post.entity';
 import {
@@ -10,24 +12,28 @@ import {
     Entity,
     ManyToMany,
     OneToMany,
+    OneToOne,
     PrimaryGeneratedColumn,
     UpdateDateColumn,
 } from 'typeorm';
 
-@Entity({ name: 'users' })
-export class User {
+@Entity()
+export class UsersModel {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @IsNotEmpty({ message: '이메일을 입력해주세요.' })
-    @IsEmail({}, { message: '이메일 형식이 올바르지 않습니다.' })
+    @IsNotEmpty()
+    @IsEmail()
     @Column({ unique: true })
     email: string;
 
-    @IsNotEmpty({ message: '비밀번호를 입력해주세요.' })
+    @IsNotEmpty()
     @IsString()
-    @MinLength(6, { message: '비밀번호는 6자 이상이어야 합니다.' })
+    @MinLength(6)
     @Column({ select: false })
+    @Exclude({
+        toPlainOnly: true,
+    })
     password: string;
 
     @IsNotEmpty({ message: '닉네임을 입력해주세요.' })
@@ -45,6 +51,12 @@ export class User {
     @Column()
     techType: string;
 
+    @Column({ default: null, nullable: true })
+    name: string;
+
+    @Column({ default: null, nullable: true })
+    contact: string;
+
     @CreateDateColumn()
     createdAt: Date;
 
@@ -54,15 +66,18 @@ export class User {
     @DeleteDateColumn()
     deletedAt: Date;
 
-    @OneToMany((type) => Comment, (comment) => comment.user)
+    @OneToOne(() => Company, (company) => company.user)
+    company: Company;
+
+    @OneToMany(() => Comment, (comment) => comment.user)
     comments: Comment[];
 
-    @OneToMany((type) => Post, (post) => post.user)
+    @OneToMany(() => Post, (post) => post.user)
     posts: Post[];
 
-    @OneToMany((type) => RecruitPost, (recruitPost) => recruitPost.user)
+    @OneToMany(() => RecruitPost, (recruitPost) => recruitPost.user)
     recruitPosts: RecruitPost[];
 
-    @OneToMany((type) => Banner, (banner) => banner.user)
+    @OneToMany(() => Banner, (banner) => banner.user)
     banners: Banner[];
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,136 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersModel } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { DUPLICATE_EMAIL, PASSWORD_NOT_MATCH } from './const/users-error-message';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-    create(createUserDto: CreateUserDto) {
-        return 'This action adds a new user';
+    constructor(
+        @InjectRepository(UsersModel)
+        private readonly usersRepository: Repository<UsersModel>,
+    ) {}
+
+    /**
+     * 이메일을 통해 유저정보 찾기
+     * @param email
+     */
+
+    async findUserAndEmail(email: string) {
+        return this.usersRepository.findOne({
+            where: {
+                email,
+            },
+        });
     }
 
-    findAll() {
-        return `This action returns all user`;
+    /**
+     * 회원가입
+     * @param CreateUserDto
+     */
+
+    async signup(createUserDto: CreateUserDto) {
+        const findUser = await this.findUserAndEmail(createUserDto.email);
+
+        if (findUser) {
+            throw new BadRequestException(DUPLICATE_EMAIL);
+        }
+
+        if (createUserDto.password !== createUserDto.passwordConfirm) {
+            throw new BadRequestException(PASSWORD_NOT_MATCH);
+        }
+
+        const hashedPassword = await bcrypt.hash(createUserDto.password, +process.env.SALT_KEY);
+
+        const newUser = await this.usersRepository.save({
+            email: createUserDto.email,
+            password: hashedPassword,
+            nickname: createUserDto.name,
+        });
+
+        return newUser;
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} user`;
+    /**
+     * ID로 유저 상세조회
+     * @param id
+     */
+    getUserById(id: number) {
+        return this.usersRepository.findOne({
+            where: {
+                id,
+            },
+        });
     }
 
-    update(id: number, updateUserDto: UpdateUserDto) {
-        return `This action updates a #${id} user`;
+    /** 유저 정보수정
+     * @Param updateUserDto
+     */
+
+    async updateUser(id: number, updateUserDto: UpdateUserDto) {
+        const user = await this.usersRepository.findOne({
+            where: {
+                id,
+            },
+        });
+
+        try {
+            Object.assign(user, updateUserDto);
+            await this.usersRepository.save(user);
+
+            return { message: 'User updated seccessfully' };
+        } catch (error) {
+            throw new BadRequestException('Failed to update user');
+        }
     }
 
-    remove(id: number) {
-        return `This action removes a #${id} user`;
+    /** 유저 상세정보 추가
+     *@param userId : number, name : string, contact : string
+     */
+
+    async updateUserDetails(userId: number, name: string, contact: string) {
+        const user = await this.usersRepository.findOne({
+            where: {
+                id: userId,
+            },
+        });
+
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+
+        user.name = name;
+        user.contact = contact;
+
+        await this.usersRepository.save(user);
     }
+
+    /**
+     * 유저 삭제(soft delete)
+     * @param userId : number
+     */
+
+    async softDeleteUser(userId: number): Promise<void> {
+        const user = await this.usersRepository.findOne({
+            where: {
+                id: userId,
+            },
+        });
+
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+
+        user.deletedAt = new Date();
+        await this.usersRepository.save(user);
+    }
+
+    /**
+     * 1. 이력서 파일업로드 (put method 만들 때 구현할것
+     * 2. 핸드폰 번호 닉네임 추가하는 부분에서 인증기능 가능하면 알아보기.
+     * 3. 이력서 다운로드 기능. 이력서 삭제기능 등록기능.
+     */
 }
