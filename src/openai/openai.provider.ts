f8794c9fc6b8d23ef5700ec91889dc190c6472bc
Luizz Lee <leehanvit511@gmail.com>
2024-01-22 17:59:50 +0900

    feat: openai 세부사항 설명.

--- src/openai/openai.provider.ts
+++ src/openai/openai.provider.ts
@@ -1,20 +1,20 @@
-if (answers.length <= 0) {
+
+    //인스턴스 생성
     const openai = new OpenAI({
         apiKey: this.configservice.get<string>('OPENAI_API_KEY')
     })
+
+    //completion 챗 생성
     const completion = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages: [
-            {role: "system", content: "answer within 200 words without using coding terms."}, 
-            {role: "user", content: question.defaultQuestion}
+            {role: "system", content: "요구사항"}, 
+            {role: "user", content: '질문'}
         ],
+        //세부사항, 정밀도, 토큰 제한, 등등
             temperature: 0.8,
             max_tokens: 64,
             top_p: 1,
     });
-    return this.answerReposiotry.save({
-        questionId: questionId,
-        defaultAnswer: completion.choices[0].message.content,
-        userId: 1
-    })
-}
+
+
