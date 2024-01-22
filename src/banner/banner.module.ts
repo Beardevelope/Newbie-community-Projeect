31bcaf198ed18deea4126a7f85edb14a5c938e9d
oorangeed <pgh9311@gmail.com>
2024-01-22 20:39:12 +0900

    fix: dev 전체 merge

--- src/banner/banner.module.ts
+++ src/banner/banner.module.ts
@@ -4,12 +4,9 @@ import { BannerController } from './banner.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Banner } from './entities/banner.entity';
 import { BannerClick } from './entities/banner-click.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Banner } from './entities/banner.entity';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Banner, BannerClick])],
-    imports: [TypeOrmModule.forFeature([Banner])],
     controllers: [BannerController],
     providers: [BannerService],
 })
