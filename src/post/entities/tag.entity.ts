7ad3107a7467ba8d88becafed3806e6eaa22e0c2
minimini1212 <zjonyv12@naver.com>
2024-01-22 17:19:05 +0900

    feat(post) post 기본 셋팅 구현

---
+++ src/post/entities/tag.entity.ts
@@ -0,0 +1,27 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import { Post } from './post.entity';
+
+@Entity()
+export class Tag {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @ManyToMany((type) => Post, (post)=> post.tags)
+    posts: Post[];
+}
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { Tag } from './entities/tag.entity';
 
 @Module({
-    controllers: [PostController],
-    providers: [PostService],
+  imports: [
+    TypeOrmModule.forFeature([Post, Tag]),
+  ],
+  controllers: [PostController],
+  providers: [PostService],
 })
 export class PostModule {}
