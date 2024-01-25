a26da203b61427437a82d3575b3ef698599a438a
Beardevelope <mck84835182@gmail.com>
2024-01-25 11:52:15 +0900

    fix : module import 순환참조 추가 error 수정, 순환참조로 인하여 userModule은 authModule을 서로 순환하여 교류

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { AccessTokenGuard, BearerTokenGuard, RefreshTokenGuard } from './guard/bearer.guard';
+import { BasicTokenGuard } from './guard/basic.guard';
 
 @Module({
     controllers: [AuthController],
-    imports: [UserModule],
-    providers: [AuthService],
-    exports: [AuthService],
+    imports: [forwardRef(() => UserModule)],
+    providers: [
+        AuthService,
+        BearerTokenGuard,
+        BasicTokenGuard,
+        AccessTokenGuard,
+        RefreshTokenGuard,
+    ],
+    exports: [AuthService, BearerTokenGuard, BasicTokenGuard, AccessTokenGuard, RefreshTokenGuard],
 })
 export class AuthModule {}
