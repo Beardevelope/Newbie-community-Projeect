9a6e4fb62439fcb123b8f1e46be1a07e3019f0e5
oorangeed <pgh9311@gmail.com>
2024-01-28 03:12:17 +0900

    fix: 댓글 좋아요 기능 수정 - UseGuard를 BearerTokenGuard로 변경 - 대댓글에 좋아요 가능하도록 경로 추가 및 entity 변경

--- src/comment-like/comment-like.controller.ts
+++ src/comment-like/comment-like.controller.ts
@@ -1,16 +1,27 @@
-import { Controller, Param, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Param, ParseIntPipe, Post, Request, UseGuards } from '@nestjs/common';
 import { CommentLikeService } from './comment-like.service';
-import { BasicTokenGuard } from 'src/auth/guard/basic.guard';
+import { BearerTokenGuard } from 'src/auth/guard/bearer.guard';
 
 @Controller('like')
-@UseGuards(BasicTokenGuard)
+@UseGuards(BearerTokenGuard)
 export class CommentLikeController {
     constructor(private readonly commentLikeService: CommentLikeService) {}
 
     // 댓글 좋아요
     @Post(':postId/:commentId')
-    async addCommentLike(@Request() req, @Param('commentId') commentId: string) {
-        const userId = req.user.id;
-        return this.commentLikeService.addCommentLike(+userId, +commentId);
+    async addCommentLike(@Request() req, @Param('commentId', ParseIntPipe) commentId: number) {
+        const userId = req.userId;
+        return this.commentLikeService.addCommentLike(userId, commentId);
+    }
+
+    // 대댓글 좋아요
+    @Post(':postId/:parentCommentId/:commentId')
+    async addReplyCommentLike(
+        @Request() req,
+        @Param('parentCommentId', ParseIntPipe) parentCommentId: number,
+        @Param('commentId', ParseIntPipe) commentId: number,
+    ) {
+        const userId = req.userId;
+        return this.commentLikeService.addCommentLike(userId, commentId);
     }
 }
