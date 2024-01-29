db09e3ea9b7af272008af014b984c43316eb7c68
Luizz Lee <leehanvit511@gmail.com>
2024-01-29 13:12:32 +0900

    feat: 서버 다운 예의 처리 추가

--- assets/Auth/js/script.js
+++ assets/Auth/js/script.js
@@ -38,48 +38,64 @@ const loginEmail = document.querySelector(".login-email");
 const loginPassword = document.querySelector(".login-password");
 
 const signup = async () => {
-    const data = {
-        email: email.value,
-        password: password.value,
-        passwordConfirm: confirmPassword.value,
-        nickname: nickname.value
+    try {   
+        const data = {
+            email: email.value,
+            password: password.value,
+            passwordConfirm: confirmPassword.value,
+            nickname: nickname.value
+        }
+        const response = await fetch(`${AUTH_API}/user/signup`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+        const responseData = await response.json()
+    
+        if (!response.ok)
+            alert(`${responseData.message}`)
+        if (response.ok) {
+            alert(`회원가입 성공`) 
+            window.location.href = "./mainpage.html"
+        }
+    } catch (error) {
+        console.error(error)
+        alert("서버 에러")
+        window.location.href = "server-error"
     }
-    const response = await fetch(`${AUTH_API}/user/signup`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-    })
-    const responseData = await response.json()
-
-    if (!response.ok) 
-        alert(`${responseData.message}`)
-    if (response.ok)
-        window.location.href = "./mainpage.html"
 }
 
 const login = async () => {
-    const data = {
-        email: email.value,
-        password: loginPassword.value,
-
+    try {
+        const data = {
+            email: email.value,
+            password: loginPassword.value,
+    
+        }
+        const credentials = btoa(`${data.email}:${data.password}`);
+        const token = `Basic ${credentials}`;
+        const response = await fetch(`${AUTH_API}/auth/login`, {
+            method: "POST",
+            headers: {
+                "Authorization": token
+            },
+        })
+        console.log(response)
+        const responseData = await response.json()
+        if (!response.ok)
+            alert(`${responseData.message}`)
+    
+        if (response.ok) {
+            alert(`로그인 성공}`)
+            window.location.href = "./mainpage.html"
+        }
+    
+    } catch (error) {
+        alert("서버 에러")
+        window.location.href = "server-error"
     }
-
-    const credentials = btoa(`${data.email}:${data.password}`);
-const token = `Basic ${credentials}`;
-    const response = await fetch(`${AUTH_API}/auth/login`, {
-        method: "POST",
-        headers: {
-            "Authorization": token
-          },
-    })
-    const responseData = await response.json()
-
-    if (!response.ok) 
-        alert(`${responseData.message}`)
-    if (response.ok)
-        window.location.href = "./mainpage.html"
 }
 
 signupButton.addEventListener('click', signup)
