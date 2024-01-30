bada75636216f3e3bcc4e0868b1c87ddc397b84f
Beardevelope <mck84835182@gmail.com>
2024-01-30 16:57:11 +0900

    new: frontend 메인화면 틀 구성,dropbox 작동 안됨

--- assets/Auth/js/mainpage.js
+++
@@ -1,9 +0,0 @@
-// Log In 버튼 클릭 이벤트
-document.getElementById('loginButton').addEventListener('click', function () {
-    window.location.href = 'auth.html#loginPage';
-});
-
-// Sign Up 버튼 클릭 이벤트
-document.getElementById('signupButton').addEventListener('click', function () {
-    window.location.href = 'auth.html#signupPage';
-});
