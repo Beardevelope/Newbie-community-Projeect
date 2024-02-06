1cbb07cffdd178d9ff642851a80b42445f785714
Beardevelope <mck84835182@gmail.com>
2024-02-07 02:01:48 +0900

    new : roleguard 수정 및 추가 로직구현, 및 ban guard  adminguard 미들웨어 추가

--- src/auth/guard/role.guard.ts
+++ src/auth/guard/role.guard.ts
@@ -1,8 +1,9 @@
 import { CanActivate, ExecutionContext, Injectable, NotAcceptableException } from '@nestjs/common';
+import { Observable } from 'rxjs';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class RoleGuard implements CanActivate {
+export class VerifyGuard implements CanActivate {
     constructor(private readonly userService: UserService) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -15,5 +16,36 @@ export class RoleGuard implements CanActivate {
         }
 
         return true;
+    }
+}
+
+export class RoleGuard implements CanActivate {
+    constructor(private readonly userService: UserService) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const req = context.switchToHttp().getRequest();
+
+        const user = await this.userService.getUserById(req.userId);
+
+        if (!user.isAdmin) {
+            throw new NotAcceptableException();
+        }
+
+        return true;
+    }
+}
+
+export class BanGuard implements CanActivate {
+    constructor(private readonly userService: UserService) {}
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const req = context.switchToHttp().getRequest();
+
+        const user = await this.userService.getUserById(req.userId);
+
+        if (user.isBan) {
+            throw new NotAcceptableException();
+        }
+
+        return false;
     }
 }
