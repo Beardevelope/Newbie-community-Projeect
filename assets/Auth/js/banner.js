f4bc177294a2eb2e1eb0944b4d4870b55dd9ef9e
oorangeed <pgh9311@gmail.com>
2024-02-07 12:04:37 +0900

    feat: 메인 배너 기본 - 배너 랜덤 노출

---
+++ assets/Auth/js/banner.js
@@ -0,0 +1,21 @@
+// 배너 랜덤으로 출력하기
+async function displayRandomBanner() {
+    try {
+        const response = await fetch('http://localhost:3000/banner/random'); // 랜덤 배너를 가져오는 API 엔드포인트 호출
+        const data = await response.json(); // JSON 형태로 변환
+
+        const bannerImageSrc = data.file; // 배너 이미지 URL
+        const bannerImageElement = document.createElement('img'); // img 엘리먼트 생성
+        bannerImageElement.src = bannerImageSrc; // 이미지 소스 설정
+        bannerImageElement.alt = 'Random Banner Image'; // 대체 텍스트 설정
+        bannerImageElement.classList.add('banner-image'); // CSS 클래스 추가
+
+        const bannerContainer = document.querySelector('.sideBanner'); // 배너가 표시될 컨테이너 선택
+        bannerContainer.innerHTML = ''; // 컨테이너 초기화
+        bannerContainer.appendChild(bannerImageElement); // 배너 이미지를 컨테이너에 추가
+    } catch (error) {
+        console.error('Error fetching random banner:', error);
+    }
+}
+
+displayRandomBanner();
