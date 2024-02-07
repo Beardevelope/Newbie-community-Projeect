0c3104e9d616cc3939ed5ff7b0c601de6313153b
oorangeed <pgh9311@gmail.com>
2024-02-08 03:01:46 +0900

    fix: 프론트 배너 url 연결 및 클릭 시 조회수 증가 구현

--- assets/Auth/js/banner.js
+++ assets/Auth/js/banner.js
@@ -1,26 +1,3 @@
-// 배너 랜덤으로 출력하기
-async function displayRandomBanner() {
-    try {
-        const response = await fetch('http://localhost:3000/banner/random'); // 랜덤 배너를 가져오는 API 엔드포인트 호출
-        const data = await response.json(); // JSON 형태로 변환
-
-        const bannerImageSrc = data.file; // 배너 이미지 URL
-        const bannerImageElement = document.createElement('img'); // img 엘리먼트 생성
-        bannerImageElement.src = bannerImageSrc; // 이미지 소스 설정
-        bannerImageElement.alt = 'Random Banner Image'; // 대체 텍스트 설정
-        bannerImageElement.classList.add('banner-image'); // CSS 클래스 추가
-
-        const bannerContainer = document.querySelector('.sideBanner'); // 배너가 표시될 컨테이너 선택
-        bannerContainer.innerHTML = ''; // 컨테이너 초기화
-        bannerContainer.appendChild(bannerImageElement); // 배너 이미지를 컨테이너에 추가
-    } catch (error) {
-        console.error('Error fetching random banner:', error);
-    }
-}
-
-displayRandomBanner();
-
-
 window.addEventListener('scroll', function () {
     var sideBanner = document.querySelector('.sideBanner');
     var scrollPosition = window.scrollY;
@@ -28,3 +5,44 @@ window.addEventListener('scroll', function () {
     // 배너가 화면 위에 고정되도록 설정합니다.
     sideBanner.style.top = (5 + scrollPosition) + 'px';
 });
+
+// 배너 클릭시 조회수 증가
+async function handleClickBanner(bannerId) {
+    try {
+        const response = await fetch(`http://localhost:3000/banner/click/${bannerId}`, {
+            method: 'POST',
+        });
+        if (response.ok) {
+            console.log('조회수 +1');
+        } else {
+            console.error('Failed to record banner click.');
+        }
+    } catch (error) {
+        console.error('Error recording banner click:', error);
+    }
+}
+
+// 배너 랜덤 출력 및 광고 페이지 연결
+async function displayRandomBanner() {
+
+    const response = await fetch('http://localhost:3000/banner/random');
+    const data = await response.json();
+
+    const bannerId = data.id;
+    const bannerImageSrc = data.file;
+    const pageUrl = data.pageUrl;
+
+    const bannerImageElement = document.createElement('img');
+    bannerImageElement.src = bannerImageSrc;
+
+    const bannerContainer = document.querySelector('.sideBanner');
+    bannerContainer.innerHTML = '';
+    bannerContainer.appendChild(bannerImageElement);
+
+    bannerContainer.addEventListener('click', function () {
+        handleClickBanner(bannerId);
+        window.open(pageUrl, '_blank');
+    });
+}
+
+displayRandomBanner();
