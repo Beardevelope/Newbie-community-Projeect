2555c8809c87512e0a36503b66c927a7668661ef
Beardevelope <mck84835182@gmail.com>
2024-02-08 18:10:31 +0900

    Co-authored-by: nohsiwon <nohsiwon@users.noreply.github.com>

--- src/mailer/mailer.module.ts
+++ src/mailer/mailer.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { EmailController } from './mailer.controller';
 import { EmailService } from './mailer.service';
 import { AuthModule } from 'src/auth/auth.module';
+
 @Module({
     imports: [AuthModule],
     controllers: [EmailController],
