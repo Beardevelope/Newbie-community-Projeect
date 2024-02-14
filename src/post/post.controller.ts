1efbb2b98c898f07ef20e144c886da61fb51616a
minimini1212 <zjonyv12@naver.com>
2024-02-14 22:58:00 +0900

    feat(post) he 라이브러리를 사용하여 xss공격 방지

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -47,7 +47,6 @@ export class PostController {
         const { order, filter, tagName, tab, page } = req.query;
         const posts = await this.postService.findAll(order, filter, tagName, tab, +page);
 
-        console.log(posts);
         return {
             statusCode: HttpStatus.OK,
             message: 'ok',
