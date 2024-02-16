315aa69b36f6fb21ce1ee8b44544c1c72d3413e0
minimini1212 <zjonyv12@naver.com>
2024-02-16 23:27:13 +0900

    feat(post) 사이드바 하단 부분 각 유저 정보 기입 구현

---
+++ assets/Post/js/mdo.js
@@ -0,0 +1,75 @@
+const accessToken = sessionStorage.getItem('accessToken');
+const dropdown = document.querySelector('.dropdown');
+
+// userId를 추출하는 함수
+function extractUserId(accessToken) {
+    if (!accessToken) {
+        console.error('토큰이 없습니다.');
+        return null;
+    }
+    try {
+        // 토큰의 페이로드(payload)를 디코딩하여 사용자 정보를 추출합니다.
+        const payload = accessToken.split('.')[1];
+        const decodedPayload = atob(payload);
+        const userInfo = JSON.parse(decodedPayload);
+        const userId = userInfo.id;
+        return userId;
+    } catch (error) {
+        console.error('토큰에서 사용자 ID를 추출하는 중 에러 발생:', error);
+        return null;
+    }
+}
+
+const userId = extractUserId(accessToken);
+
+async function findByUserId() {
+    try {
+        const response = await fetch(`http://localhost:3000/user/by-userId/${userId}`, {
+            accept: 'application/json',
+        });
+        const jsonData = await response.json();
+
+        dropdown.innerHTML = `<a
+                        href="#"
+                        class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                        >
+                            <img
+                                src="${jsonData.profileImage}"
+                                alt=""
+                                width="32"
+                                height="32"
+                                class="rounded-circle me-2"
+                            />
+                            <strong>${jsonData.nickname}</strong>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+                            <li><a class="dropdown-item" href="#">New project...</a></li>
+                            <li><a class="dropdown-item" href="#">Settings</a></li>
+                            <li><a class="dropdown-item" href="../../Auth/mypage.html">Profile</a></li>
+                            <li><hr class="dropdown-divider" /></li>
+                            <li><a class="dropdown-item" id="signOut">Sign out</a></li>
+                        </ul>`;
+        addEventListenerSignOut();
+    } catch (error) {
+        console.log(error.message);
+        alert(error.message);
+    }
+}
+if (userId) {
+    findByUserId();
+}
+
+function addEventListenerSignOut() {
+    const signOut = document.getElementById('signOut');
+
+    signOut.addEventListener('click', function () {
+        sessionStorage.removeItem('accessToken');
+        loginButton.style.display = 'inline-block';
+        signupButton.style.display = 'inline-block';
+        logoutButton.style.display = 'none';
+        alert('로그아웃 하였습니다.')
+        window.location.reload();
+    });
+}
